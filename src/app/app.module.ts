997b4aa098710e71c0382cd27d69454d503fdd86
durga-achu <durgava2665@gmail.com>
2021-04-13 11:43:54 -0700

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProComponent } from './pro/pro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
